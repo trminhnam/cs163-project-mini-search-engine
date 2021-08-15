2342e7d928c0cb8cd2146eae1614a9a4d2b2c6de
vhyen <81974003+vhyen@users.noreply.github.com>
2021-08-15 16:13:46 +0700

    Header File & Comments

--- Project/main/ConsoleAndOthers.h
+++ Project/main/ConsoleAndOthers.h
@@ -59,55 +59,6 @@ void clearHistory(HANDLE& h);
 
 
 //Heading
-inline void heading(HANDLE& hStdout) {
-	system("cls");
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	int columns = 172, rows = 45;
-	
-	GetConsoleScreenBufferInfo(hStdout, &csbi);
-	columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-	rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
 
-
-	//cout << columns << " " << rows << endl;
-	int mid = 2;
-
-	string title = "MINI SEARCH ENGINE BY GROUP 6 - 20APCS1";
-
-	//Fist make title
-	string res = "|";
-	for (int i = 0; i < columns / 2 - title.size() / 2 + 1; i++) {
-		res += " ";
-	}
-	res += title;
-	for (int i = res.size(); i < columns; i++) {
-		if (i == columns - 1)
-			res += "|";
-		else
-			res += " ";
-	}
-
-
-	//Then print out the limits and the title
-	for (int i = 0; i < columns; i++)
-		cout << "~";
-	for (int i = 0; i < mid * 2 + 1; i++) {
-		if (i == mid)
-			cout << res;
-		else
-			for (int j = 0; j < columns; j++) {
-				if (j == 0 || j == columns - 1)
-					cout << "|";
-				else
-					cout << " ";
-			}
-	}
-	for (int i = 0; i < columns; i++)
-		cout << "~";
-
-	cout << endl << endl << endl;
-	//COORD cur = GetConsoleCursorPosition(hStdout);
-	//SetConsoleCursorPosition(hStdout, cur);
-}
 
 #endif // !_CONSOLEANDOTHERS_H_
